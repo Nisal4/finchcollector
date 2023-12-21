8162455f928d18c5353f1bd9bc47a285bf3dea8a
root <root@DESKTOP-PV5G1DG.>
2023-12-20 23:26:32 -0500

    Add finch dictionary

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
+finches = [
+    {"id": 1, "species": "Zebra Finch", "color": "Black and White", "size": "Small"},
+    {"id": 2, "species": "Gouldian Finch", "color": "Multi-colored", "size": "Small"},
+    {"id": 3, "species": "Bengalese Finch", "color": "Brown and White", "size": "Small"}
+]
 def home(request):
     return render(request, 'home.html')
 
@@ -9,4 +14,6 @@ def about(request):
 
 def finches_index(request):
     return render(request, 'finches/index.html')
+
+
 
